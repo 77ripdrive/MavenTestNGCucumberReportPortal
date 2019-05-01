f07434af36be4ae40b33a1fd9868837e72f5d703
Gleb Telesh <77ripdrive@gmail.com>
2019-05-01 11:31:27 +0300

    -Java ScriptEx

--- src/test/java/page/TenMinuteMailPage.java
+++ src/test/java/page/TenMinuteMailPage.java
@@ -2,36 +2,33 @@ package page;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.openqa.selenium.JavascriptExecutor;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.concurrent.TimeUnit;
 
 public class TenMinuteMailPage extends AbstractPage {
-    public final String BASE_URL = "https://10minutemail.net";
+    private final String BASE_URL = "https://10minutemail.net";
     private final Logger logger = LogManager.getRootLogger();
-    Actions actions = new Actions(driver);
+    private Actions actions = new Actions(driver);
     public TenMinuteMailPage(WebDriver driver) {
         super(driver);
         PageFactory.initElements(this.driver, this);
     }
 
     @FindBy(xpath = "//*[@id='fe_text']")
-    public WebElement emailFromTenMinute;
+    protected  WebElement emailFromTenMinute;
     @FindBy(xpath = "//*[@id='maillist']/tbody/tr[2]/td[1]")
-    public WebElement tenMinuteMail;
+    protected WebElement tenMinuteMail;
     @FindBy(xpath = "//*[@id='tab1']//table/tbody/tr[2]/td[2]/h3")
-    public WebElement mailTotalEstCost;
+    protected WebElement mailTotalEstCost;
 
     public  String getemailFromTenMinute() {
-          String emailFromTenMinuteMail = emailFromTenMinute.getAttribute("value");
-    return emailFromTenMinuteMail;
+          return emailFromTenMinute.getAttribute("value");
     }
 
     public String readTotalEstimateCostFromTenMinute() {
@@ -41,13 +38,11 @@ public class TenMinuteMailPage extends AbstractPage {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-//        new WebDriverWait(driver, 80)
-//                .until(ExpectedConditions.elementToBeClickable(tenMinuteMail));
-        logger.info("....");
+
         actions.moveToElement(tenMinuteMail);
         tenMinuteMail.click();
         String mailTotalEstimateCostFromTenMinuteEmail = mailTotalEstCost.getText();
-        logger.info("Email from GoogleCloud with Total Coast");
+        logger.info("Email from GoogleCloud with Total Coast is checked");
         return mailTotalEstimateCostFromTenMinuteEmail;
     }
     @Override
